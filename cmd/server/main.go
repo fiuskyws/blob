7da13d57410c1e85759a03955e4fa4e09d71a750
Rafael Breno <rafael_breno@outlook.com>
2024-01-04 19:38:14 -0300

    feat: add base main.go

---
+++ cmd/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.uber.org/zap"
+)
+
+func main() {
+	l, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO:
+	// 	- Remove this implementation, MUST NOT use `ReplaceGlobals`
+	undo := zap.ReplaceGlobals(l)
+	defer undo()
+
+	exit := make(chan os.Signal, 1)
+
+	signal.Notify(exit, syscall.SIGTERM, syscall.SIGINT)
+
+	<-exit
+}
